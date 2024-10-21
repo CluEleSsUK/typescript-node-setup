3cae3876accc0af7004e83ac66da4369061cea34
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-10-21 10:27:51 +0200

    updated more stuff

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", {
+      useESM: true,
+    }],
+  },
 };
+
