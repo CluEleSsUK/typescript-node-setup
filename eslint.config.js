1f24c53148188ee8ce920e8f407245cf4b3231dd
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-11-08 13:47:20 +0100

    updated ts node setup

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,6 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
-    {"ignores": ["src/generated/**"]}
+    {"ignores": ["src/generated/**", "dist/**"]}
 );
 
